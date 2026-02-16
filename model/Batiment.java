2c7f638ea218866b27b487c04d2708bc319711de
tofulecode <axelomichaux@gmail.com>
2026-02-16 15:38:57 +0100

    Classe Générale Batiments

--- model/Batiment.java
+++ model/Batiment.java
@@ -2,5 +2,23 @@ package model;
 
 // Classe générale des bâtiments dans le jeu
 public class Batiment {
+
+    // Attributs communs à tous les bâtiments
+    protected String nom;
     
+    protected Boolean estConstruit;
+
+    protected int coutBois;
+    protected int coutPierre;
+    protected int coutOr;
+
+    // Constructeur de la classe Batiment
+    public Batiment(String nom, int coutBois, int coutPierre, int coutOr) {
+        this.nom = nom;
+        this.coutBois = coutBois;
+        this.coutPierre = coutPierre;
+        this.coutOr = coutOr;
+        this.estConstruit = false; // Par défaut, le bâtiment n'est pas construit
+    }
+
 }

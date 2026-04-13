10eecdb7e06bf9200e393e0238501f34a600b25b
Julien Denis <julienjmdenis@gmail.com>
2026-04-13 14:58:21 +0200

    Modify day/night cycle durations in JourNuit
    Updated the simulation to alternate between day and night with specific durations.

--- src/model/JourNuit.java
+++ src/model/JourNuit.java
@@ -4,7 +4,7 @@ public class JourNuit extends Thread {
     private boolean isDay;
 
     public JourNuit() {
-        this.isDay = true; // Commence la simulation avec la nuit
+        this.isDay = true; // Commence le jeu avec le jour
     }
 
     public boolean getIsDay() {
@@ -14,14 +14,16 @@ public class JourNuit extends Thread {
     @Override
     public void run() {
         while (true) {
-            isDay = !isDay; // Alterne entre jour et nuit
             if (isDay) {
                 System.out.println("Le jour se lève.");
             } else {
                 System.out.println("La nuit tombe.");
             }
             try {
-                Thread.sleep(30000); // on change toutes les 30s (ajustable)
+                Thread.sleep(120000); // 2 minutes de jour
+                isDay = !isDay;
+                Thread.sleep(30000); // 30 secondes de nuit
+                isDay = !isDay;
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 break;
